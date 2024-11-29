e2e4655387acbf382532df5063f97bb06a9bdd3d
OUlmachi-Yassir <oulmachi.yassir@gmail.com>
2024-11-29 16:51:04 +0100

    add: github actions for my backend

--- BACKEND/src/routes/auth.js
+++ BACKEND/src/routes/auth.js
@@ -4,7 +4,7 @@ const jwt = require('jsonwebtoken');
 const User = require('../model/User');
 const router = express.Router();
 
-const JWT_SECRET = process.env.JWT_SECRET || 'your_secret_key';
+const JWT_SECRET = process.env.JWT_SECRET;
 
 const authMiddleware = (roles = []) => {
   return (req, res, next) => {
